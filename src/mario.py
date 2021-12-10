abc718dbad0e67832f472eacd83b5c1701b7d382
random10101 <100475820@alumnos.uc3m.es>
2021-12-10 22:29:31 +0100

    v1 sistema de colisiones mario

--- src/mario.py
+++ src/mario.py
@@ -10,6 +10,13 @@ class Mario:
         self.__y = 200
         self.__vx = 0
         self.__vy = 0
+        self.height = 16
+        self.width = 16
+
+        self.jumping = False
+        self.double_jumping = False
+        self.distance_to_floor = 200
+        self.reset_collisions()
 
     @property
     def x(self):
@@ -23,33 +30,66 @@ class Mario:
     def y(self):
         return self.__y
 
+    def reset_collisions(self):
+        # Resetear el sistema de colisiones
+        self.wall_top = (False, 0)
+        self.wall_bottom = (False, 0)
+        self.wall_left = (False, 0)
+        self.wall_right = (False, 0)
+
+    def reset_distance_to_floor(self):
+        self.distance_to_floor = 200
+
     def update(self):
+        self.reset_collisions()
         self.move()
         self.jump()
 
     def move(self):
-        # Intervalo válido x: (0, pyxel.height/2)
         if pyxel.btn(pyxel.KEY_LEFT):
-            self.__vx = max(self.__vx-1, -8)
+            self.__vx = max(self.__vx-1, -4) 
             self.__x = max(self.__x+self.__vx, 0)
         elif pyxel.btn(pyxel.KEY_RIGHT):
-            self.__vx = min(self.__vx+1, 8)
+            self.__vx = min(self.__vx+1, 4) 
             self.__x = min(self.__x+self.__vx, pyxel.height/2)
         else:
             self.__vx = 0
     
     def jump(self):
-        # Intervalo válido y: (0, 200)
-        if pyxel.btn(pyxel.KEY_SPACE):
-            self.__vy = max(self.__vy-2, -8)
-        elif self.__y >= 200:
+        if pyxel.btn(pyxel.KEY_SPACE) and not self.double_jumping:
+            self.__vy = max(self.__vy-6, -12)
+            if self.jumping:
+                self.double_jumping = True
+            else:
+                self.jumping = True
+
+        elif self.__y >= self.distance_to_floor:
             self.__vy = 0
+            if self.double_jumping:
+                self.double_jumping = False
+            else:
+                self.jumping = False
+
         else: 
             self.__vy = min(self.__vy + 1, 8)
-        self.__y = min(self.__y+self.__vy, 200) 
+        self.__y = min(self.__y+self.__vy, self.distance_to_floor) 
     
-    def Chocar(self, objeto: BloqueInterrogación):
-        if objeto.x >= self.__x and objeto.x + 8 <= self.__x:
-            self.__vx = 0
+    def touch_bottom(self, y):
+        # Evento de choque por abajo
+        self.__y = min(self.y, y+self.height)
+        self.wall_bottom = (True, y)
+        self.distance_to_floor = y - 16
 
-    
+    def touch_right(self, x):
+        # Evento de choque por la derecha
+        self.__x = x - self.width
+        self.wall_right = (True, x)
+
+    def touch_left(self, x):
+        # Evento de choque por la izquierda
+        self.__x = x - self.width
+        self.wall_left = (True, x)
+
+    def touch_top(self):
+        # Evento de choque por arriba
+        pass 
